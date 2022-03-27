9cae965e9d175abfac10c9000967770c7abc460a
Dmitriy-Stepanenko <d.step.co@gmail.com>
2022-03-27 19:32:52 +0300

    fixed mistake
    fixed mistake

--- dbt/models/postgres/covid_total_data.sql
+++ dbt/models/postgres/covid_total_data.sql
@@ -1,7 +1,7 @@
 select
     sum((s.json_data ->> 'Confirmed')::int) as total_confirmed
   , sum((s.json_data ->> 'Deaths')::int) as total_deaths
-  , (sum((s.json_data ->> 'Confirmed')::int) / sum(p.population::float) * 100)::numeric(38,2) as affested_share
+  , (sum((s.json_data ->> 'Confirmed')::int) / sum(p.population::float) * 100)::numeric(38,2) as affected_share
   , (sum((s.json_data ->> 'Deaths')::int) / sum((s.json_data ->> 'Confirmed')::float) * 100)::numeric(38,2) as mortality_rate
 from
     {{ source('covid', 'stats')}} s
